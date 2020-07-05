434dfd39ce7c69ad3eebab09870416734205e0a7
AveWycc220 <mik16748@gmail.com>
2020-07-05 03:08:50 +0300

    feat (src): create events on button.show and button.delete

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import {findData, findButton, addTask, confirmTask} from './toDo.js'
 
+let COUNT = 0
+
 const data = findData()
 const buttonAdd = findButton("#add")
 let addInfo = document.querySelector("#addInfo")
 let isTaskAdded = true
 let buttonConfirm = undefined
+let buttonShowList = []
+let buttonShowDelete = []
 
 buttonAdd.onclick = () => {
     if (isTaskAdded) {
@@ -14,13 +18,39 @@ buttonAdd.onclick = () => {
         isTaskAdded = false
         buttonConfirm.onclick = () => {
             confirmTask(data)
+            COUNT += 1
             isTaskAdded = true
+            let buttonShow = document.querySelector(`#show_${COUNT}`)
+            buttonShow.addEventListener("click", () => {
+                // TODO Create function ShowTask
+                console.log(buttonShow)
+            })
+            buttonShowList.push(buttonShow)
+            let buttonDelete = document.querySelector(`#delete_${COUNT}`)
+            buttonDelete.addEventListener("click", () => {
+                // TODO Create function DeleteTask
+                console.log(buttonDelete)
+            })
+            buttonShowDelete.push(buttonDelete)
         }
     } else {
         addInfo.innerHTML = "Confirm your task before you will add new"
         buttonConfirm.onclick = () => {
             confirmTask(data)
+            COUNT += 1
             isTaskAdded = true
+            let buttonShow = document.querySelector(`#show_${COUNT}`)
+            buttonShow.addEventListener("click", () => {
+                 // TODO Create function ShowTask
+                console.log(buttonShow)
+            })
+            buttonShowList.push(buttonShow)
+            let buttonDelete = document.querySelector(`#delete_${COUNT}`)
+            buttonDelete.addEventListener("click", () => {
+                 // TODO Create function DeleteTask
+                console.log(buttonDelete)
+            })
+            buttonShowDelete.push(buttonDelete)
         }
     }
 }
